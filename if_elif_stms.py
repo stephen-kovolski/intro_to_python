da9e8882e280d2a6198124b23b1ad4acdd900097
stephen-kovolski <s.kovolski@gmail.com>
2020-10-02 22:29:34 -0400

    if_elif_stmts

---
+++ if_elif_stms.py
@@ -0,0 +1,11 @@
+name = 'Bob'
+age = 3000
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice')
+elif age > 2000:
+    print('How can you be so old?')
+elif age > 100:
+    print > 100
