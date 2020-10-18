3214f08da99dec4747e1d817ece61f24df846170
stephen-kovolski <s.kovolski@gmail.com>
2020-10-17 23:30:17 -0400

    python standard library

---
+++ built_in_functions.py
@@ -0,0 +1,40 @@
+#import modules with import statements
+
+import random
+
+#random.randint(1, 10)
+
+print(random.randint(1, 10)
+)
+
+
+import random, sys, os, math
+#import multiple module by using commas.
+
+from random import *
+#the dot means all, so this line is importin all from random.  This allows us to not have to use random.  to use the functions like above.  It would look like this,
+
+print(randint(1, 10))
+
+#its better to use the fist example becayse we can alwasy find wher the randint function lives.  That would be random in this case.  
+
+
+
+
+
+
+#sys.exit() will cancel a program early if you want/need it to stop at a certain point
+
+import sys
+
+print('hello')
+sys.exit()
+print('goodbye')
+
+
+
+
+#There are also 3rd party programs kind of like npm.  the pip progam is like npm.
+
+
+
--- forLoops.py
+++ forLoops.py
@@ -1,3 +1,6 @@
 print('My name is')
 for i in range(5):
     print("Jimmy Five Times" + str(i))
+
+
+    #in the range function 1 parametr will go from i to the parameter.  If there are 2 aeguments the loop will start at the first argument and go to the second argument but not invludr it in theout out. If there are 3 arguments, it starts at the first argument, goes to the second arguments, and the third argument is how much the loop iterates.  if the 3rd arguments is 2 the loop iterates by 2.  Same goes for negative numbers/
